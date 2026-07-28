b402770a819fda8c2b2c1389050aed3249ef3d2b
aakritiiyadav <yadavaakriti2007@gmail.com>
2026-07-28 14:32:50 +0530

    Time: 4 ms (89.86%), Space: 47 MB (89.86%) - LeetHub

---
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public boolean canConstruct(String s, int k) {
+
+        if (k > s.length())
+            return false;
+
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray())
+            freq[ch - 'a']++;
+
+        int odd = 0;
+
+        for (int count : freq)
+            if (count % 2 == 1)
+                odd++;
+
+        return odd <= k;
+    }
+}
